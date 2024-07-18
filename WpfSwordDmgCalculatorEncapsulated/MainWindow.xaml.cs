3f630e8f74edde1607432ab29a41e33ec59313f2
illiated <illiated@gmail.com>
2024-07-18 17:04:38 +0100

    Add project files.

---
+++ WpfSwordDmgCalculatorEncapsulated/MainWindow.xaml.cs
@@ -0,0 +1,64 @@
+﻿using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WpfSwordDmgCalculatorEncapsulated
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        Random random = new Random();
+        SwordDamage swordDamage;
+        public MainWindow()
+        {
+            InitializeComponent();
+            swordDamage = new SwordDamage(random.Next(1, 7) + random.Next(1, 7) + random.Next(1, 7));
+            DisplayDamage();
+        }
+        public void RollDice()
+        {
+            swordDamage.Roll = random.Next(1, 7) + random.Next(1, 7) + random.Next(1, 7);
+            DisplayDamage();
+        }
+        void DisplayDamage()
+        {
+            damage.Text = $"Rolled {swordDamage.Roll} for {swordDamage.Damage} HP";
+        }
+        private void RollDamage_Click(object sender, RoutedEventArgs e)
+        {
+            RollDice();
+        }
+        private void Flaming_Checked(object sender, RoutedEventArgs e)
+        {
+            swordDamage.Flaming = true;
+            RollDice();
+        }
+
+        private void Flaming_Unchecked(object sender, RoutedEventArgs e)
+        {
+            swordDamage.Flaming = false;
+            RollDice();
+        }
+
+        private void Magic_Checked(object sender, RoutedEventArgs e)
+        {
+            swordDamage.Magic = true;
+            RollDice();
+        }
+
+        private void Magic_Unchecked(object sender, RoutedEventArgs e)
+        {
+            swordDamage.Magic= false;
+            RollDice();
+        }
+    }
+}
---
+++ WpfSwordDmgCalculatorEncapsulated/SwordDamage.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Reflection.Metadata.Ecma335;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WpfSwordDmgCalculatorEncapsulated
+{
+    class SwordDamage
+    {
+        /// <summary>
+        /// This constructor take an intial 3d6 roll right at the begninning before things kick off. And it also calculates the damage right off the bat.
+        /// This calculates damage with default magic and flaming values.
+        /// </summary>
+        /// <param name="initialRoll"></param>
+        public SwordDamage(int initialRoll) 
+        {
+            roll = initialRoll;
+            CalculateDamage();
+        }
+        private const int BASE_DAMAGE = 3;
+        private const int FLAME_DAMAGE = 2;
+
+        private int roll;
+        /// <summary>
+        /// This Roll property returns the value of the 3d6 roll. It also calculates the damage
+        /// </summary>
+        public int Roll
+        {
+            get { return roll; }
+            set { 
+                  roll = value;
+                  CalculateDamage();
+                }
+            
+        }
+        /// <summary>
+        /// This auto property returns the value of damage. Its set accessor for updating the damage value is private.
+        /// </summary>
+        public int Damage { get; private set; }
+        private void CalculateDamage()
+        {
+            decimal magicMultiplier = 1M;
+            if (Magic)
+            {
+                magicMultiplier = 1.75M;
+            }
+            Damage = (int)(Roll * magicMultiplier) + BASE_DAMAGE;
+            Debug.WriteLine($"Rolled {Roll} for damage {Damage}.");
+            if (Flaming)
+            {
+                Damage += FLAME_DAMAGE;
+            }
+            Debug.WriteLine($"Rolled {Roll} for damage {Damage}.");
+    }
+        private bool magic;
+        /// <summary>
+        /// Magic property returns if its magic or not (true or false). It also calculates the damage afterwards
+        /// </summary>
+        public bool Magic
+        {
+            get { return magic; }
+            set
+            {
+                magic = value;
+                CalculateDamage();
+            }
+        }
+        private bool flaming;
+        /// <summary>
+        /// Flaming property checks the condition if its flaming or not (true or false). It also calculates the damage afterwards.
+        /// </summary>
+        public bool Flaming
+        {
+            get { return flaming; }
+            set
+            {
+                flaming = value;
+                CalculateDamage();
+            }
+        }
+      
+    }
+}
